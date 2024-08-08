161c422bd27aa1328f3504e45fccfe38ff7e91f7
Andres D <84483578+fmcubium@users.noreply.github.com>
2024-08-07 20:18:57 -0400

    Implemented algorithm for creating league match calendar within test code

--- Tournaments/league.py
+++ Tournaments/league.py
@@ -1,4 +1,40 @@
+import copy
+
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
 import pandas as pd
+
+
+def create_fixtures(opp_codes: list):
+    opp_codes_clone = copy.deepcopy(opp_codes)
+    first_round = []
+    rng = np.random.default_rng()
+
+    half = len(opp_codes_clone) // 2
+    rng.shuffle(opp_codes_clone)
+
+    # split the teams into halves so we can run circle method:
+    # https://en.wikipedia.org/wiki/Round-robin_tournament#Circle_method
+    left = opp_codes_clone[:half]
+    right = opp_codes_clone[half:][::-1]
+    for i in range(len(opp_codes_clone) - 1):
+        # pair
+        matchweek = list(zip(left, right))
+
+        # rotate - 1st in right goes to 2nd in left, last in left becomes last in right
+        left.insert(1, right.pop(0))
+        right.append(left.pop())
+
+        # add matchweek to fixture list
+        first_round.append(matchweek)
+
+    second_round = [[(away, home) for (home, away) in day] for day in first_round]
+
+    # shuffle both rounds and return them together
+    rng.shuffle(first_round)
+    rng.shuffle(second_round)
+
+    return first_round + second_round
 
 
 def simulate(*args: str):
@@ -29,17 +65,15 @@ def simulate(*args: str):
     print(team_codes)
     print(opp_codes)
 
-    # Go through opp codes and match w each other
-    matches = []
-    for i in range(len(opp_codes)):
-        for j in range(len(opp_codes)):
-            if i != j:
-                matches.append((opp_codes[i], opp_codes[j]))
+    fixtures = create_fixtures(opp_codes)
 
-    print(len(matches))  # should be 380
-    print("\n")
-    print(matches)
+    print()
+    for i in range(len(fixtures)):
+        print(fixtures[i])
 
     # PHASE 2: Fit our model using our data - use the data for teams that are in the league
 
     # PHASE 3: Run predictions and assign points, to determine our league ranking
+
+
+simulate()
