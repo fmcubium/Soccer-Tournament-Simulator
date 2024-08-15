363df452e134c1784ce983f108d3d019dc6dc383
Andres D <84483578+fmcubium@users.noreply.github.com>
2024-08-15 16:39:20 -0400

    More progress + replaced data with more descriptive data

--- main.py
+++ main.py
@@ -12,12 +12,25 @@ def main():
     # Program loop
     on = True
     while on:
-        sel1 = int(input("Enter: 1. Choose tournament type 2. Exit \n"))
-        match sel1:
-            case 1:
-                print("this is a temporary insertion statement weeeeeeeee")
-            case 2:
-                on = False
+        try:
+            sel1 = int(input("Enter: 1. Choose tournament type 2. Exit \n"))
+            match sel1:
+                case 1:
+                    print("Now, choose your tournament format. \n"
+                          "A league contains an even number of teams and all the teams play each other twice. Whoever "
+                          "scores the most points wins. \n"
+                          "A knockout contains 2^n (where n is an integer) teams. Teams are put into a bracket based on"
+                          "seeding. The losers of each game are knocked out until only one team remains. \n"
+                          "A group knockout contains 2^n (where n is an integer) teams. Teams are first divided into "
+                          "groups that play similar to a league format and then the top two of each group move on to "
+                          "the"
+                          "knockout.\n\n"
+                          "See the README for more details.\n\n")
+                    sel2 = int(input("Enter: 1. League 2. Knockout 3. Group & Knockout \n"))
+                case 2:
+                    on = False
+        except ValueError:
+            print("Input error. Resetting...")
 
 
 if __name__ == '__main__':
